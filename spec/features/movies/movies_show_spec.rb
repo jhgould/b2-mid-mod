efdc125de0322cc12bf8b57e4e0c0d25b7029c6d
Jack Gould <johnhilsmangould@gmail.com>
2020-05-22 09:48:05 -0600

    add test or US 2

---
+++ spec/features/movies/movies_show_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe "Movies show page", type: :feature do
+  before :each do
+    @studio1 = Studio.create!(name: "Warners", location: "Hollywood")
+    @studio2 = Studio.create!(name: "Disney", location: "LA")
+    @cars = @studio1.movies.create!(title: "Cars", creation_year: 2000, genre: "family")
+    @LOR = @studio1.movies.create!(title: "Lord if the Rings", creation_year: 2005, genre: "action")
+    @FAF = @studio2.movies.create!(title: "Fast and Furious", creation_year: 2020, genre: "action")
+    @gimly = Actor.create!(name: "Gimly", age: 150)
+    @frodo = Actor.create!(name: "Frodo", age: 50)
+    @aragorn = Actor.create!(name: "Aragorn", age: 30)
+    @owen = Actor.create!(name: "Ownen Wilson", age: 50)
+    @larry = Actor.create!(name: "Larry", age: 55)
+    @paul = Actor.create!(name: "Paul Walker", age: 30)
+  end
+
+  it "Visit movies show page and see all info including actors" do
+    visit "/movies/#{@LOR.id}"
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+# Story 2
+# As a visitor,
+# When I visit a movie's show page.
+# I see the movie's title, creation year, and genre,
+# and a list of all its actors from youngest to oldest.
+# And I see the average age of all of the movie's actors
